8d31d9ae2a7abb2c2dba9721fce89d59ce46babe
Jonas <jonas.dongowski@gmail.com>
2023-12-10 19:18:53 +0100

    Added Vue client project

---
+++ build-for-windows-amd64.ps1
@@ -0,0 +1,1 @@
+$env:CGO_ENABLED=1; $env:GOOS="windows"; $env:GOARCH="amd64"; go build .
--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base32"
 	"log"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/gofiber/template/html/v2"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"gorm.io/gorm/schema"
-	"nmbr.one/big-bans/mojangapi"
 )
 
 type Ban struct {
@@ -21,142 +20,75 @@ type Ban struct {
 	Duration   time.Duration
 }
 
-type Connection struct {
+type Session struct {
 	gorm.Model
 	PlayerUUID   string
 	LastActiveAt time.Time
 	Token        string
 }
 
-type IndexView struct {
-	Token string
+type CreateSession struct {
+	PlayerUUID string `json:"playerUniqueId"`
 }
 
-type BansView struct {
-	Bans []BansViewBan
+type CreatedSession struct {
+	Token string `json:"token"`
 }
 
-type BansViewBan struct {
-	Ban
-	CreatedAtF string
-	BannedName string
-	BannerName string
-}
-
-type FrameView struct {
-	PlayerUUID string
+func generateRandomString(length int) string {
+	b := make([]byte, length)
+	_, err := rand.Read(b)
+	if err != nil {
+		panic(err)
+	}
+	return base32.StdEncoding.EncodeToString(b)
 }
 
 func main() {
-	dsn := "root:localroot@tcp(127.0.0.1:3999)/bigbans?charset=utf8mb4&parseTime=True&loc=Local"
-	db, dberr := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		NamingStrategy: schema.NamingStrategy{
-			SingularTable: false,
-			NoLowerCase:   true,
-		},
-	})
+	// dsn := "root:localroot@tcp(127.0.0.1:3999)/bigbans?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, dberr := gorm.Open(mysql.Open(dsn), &gorm.Config{
+	// 	NamingStrategy: schema.NamingStrategy{
+	// 		SingularTable: false,
+	// 		NoLowerCase:   true,
+	// 	},
+	// })
+
+	db, dberr := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 
 	if dberr != nil {
 		panic("Failed to connect to local database")
 	}
 
 	db.AutoMigrate(&Ban{})
-	db.AutoMigrate(&Connection{})
+	db.AutoMigrate(&Session{})
 
-	engine := html.New("./views", ".html")
-
-	app := fiber.New(fiber.Config{
-		Views: engine,
-	})
+	app := fiber.New(fiber.Config{})
 
 	app.Use(logger.New())
 	app.Use(cors.New())
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		token := c.Query("token")
+	app.Static("/", "public")
 
-		return c.Render("index", IndexView{
-			Token: token,
-		})
-	})
+	app.Post("/api/sessions", func(c *fiber.Ctx) error {
+		var body = new(CreateSession)
+		var bodyError = c.BodyParser(&body)
 
-	app.Get("/bans", func(c *fiber.Ctx) error {
-		var token = c.Query("token")
-		var connections []Connection
-
-		db.Where("Token = ?", token).Find(&connections)
-
-		if len(connections) > 0 {
-			var con = connections[0]
-
-			if !time.Now().After(con.LastActiveAt.Add(time.Hour * 1)) {
-				con.LastActiveAt = time.Now()
-
-				db.Save(con)
-
-				// Session good
-				var bans []Ban
-				var viewBans []BansViewBan
-
-				db.Find(&bans)
-
-				for _, ban := range bans {
-					bannedName, err := mojangapi.GetNameFromUUID(ban.BannedUUID)
-
-					if err != nil {
-						bannedName = "ERROR: " + err.Error()
-					}
-
-					bannerName, err := mojangapi.GetNameFromUUID(ban.BannerUUID)
-
-					if err != nil {
-						bannerName = "ERROR: " + err.Error()
-					}
-
-					var viewBan = BansViewBan{
-						Ban:        ban,
-						CreatedAtF: ban.CreatedAt.Format(time.RFC1123),
-						BannedName: bannedName,
-						BannerName: bannerName,
-					}
-
-					viewBans = append(viewBans, viewBan)
-				}
-
-				return c.Render("bans", BansView{
-					Bans: viewBans,
-				})
-			}
+		if bodyError != nil {
+			return c.SendStatus(fiber.StatusBadRequest)
 		}
 
-		return c.Render("invalid_session", nil)
-	})
-
-	app.Post("/logout", func(c *fiber.Ctx) error {
-		var body = struct {
-			Token string `json:"token"`
-		}{}
-		var bodyerror = c.BodyParser(&body)
-
-		if bodyerror != nil {
-			return c.SendStatus(400)
+		var session = Session{
+			PlayerUUID: body.PlayerUUID,
+			Token:      generateRandomString(32),
 		}
 
-		var connections []Connection
+		db.Save(&session)
 
-		db.Where("Token = ?", body.Token).Find(&connections)
-
-		if len(connections) > 0 {
-			var con = connections[0]
-
-			if !time.Now().After(con.LastActiveAt.Add(time.Hour * 1)) {
-				db.Delete(&con)
-
-				return c.Render("logged_out", nil)
-			}
+		var createdSession = CreatedSession{
+			Token: session.Token,
 		}
 
-		return c.SendStatus(401)
+		return c.JSON(createdSession)
 	})
 
 	log.Fatal(app.Listen(":3000"))
